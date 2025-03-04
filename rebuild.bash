0256860a61611ad4c1767a6729bc1e26e5e21ddb
Paul Tarvydas <paul.tarvydas@gmail.com>
2025-03-04 11:55:57 -0500

    tweak rebuild.bash

--- rebuild.bash
+++ rebuild.bash
@@ -2,7 +2,7 @@
 # compile and run larson.drawio - this DPL program will compile each worker component for Larson and will compile scanner.drawio, then display a single file containing all of he this code
 ./zd/lu.py "1. node"
 node das2json.js <scanner.drawio >scanner.drawio.json
-node das2json.js <larson-html.drawio >larson-html.drawio.json
+node das2json.js <code-generator.drawio >code-generator.drawio.json
 ./zd/lu.py "2. python3"
-python3 main.py . - '' main larson-html.drawio.json
+python3 main.py . - '' main code-generator.drawio.json
 ./zd/lu.py "3. rebuild embed done"
